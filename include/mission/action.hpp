9adc120f2847b2013a1386fcd5a0a71aa7e5e202
Luke Shimanuki <luke.shimanuki@gmail.com>
2016-04-02 15:29:34 -0700

    implement action base class and example subclass

--- include/mission/action.hpp
+++ include/mission/action.hpp
@@ -1,5 +1,25 @@
 #ifndef MISSION_ACTION_HPP
 #define MISSION_ACTION_HPP
 
+#include "model/matrix.hpp"
+
+class Action
+{
+public:
+	virtual bool run(FILE*, FILE*) = 0;
+};
+
+class MoveTo : public virtual Action
+{
+public:
+	MoveTo(const Matrix&, float);
+
+	virtual bool run(FILE*, FILE*);
+
+private:
+	Matrix target;
+	float minDistance;
+};
+
 #endif
 
--- src/mission/action.cpp
+++ src/mission/action.cpp
@@ -1,4 +1,27 @@
 #include "mission/action.hpp"
 
+#include "mission/command.hpp"
+#include "mission/query.hpp"
 
+MoveTo::MoveTo(const Matrix& target, float minDistance) :
+	target(target),
+	minDistance(minDistance)
+{
+}
+
+bool MoveTo::run(FILE* in, FILE* out)
+{
+	bool close = false;
+	while (!close)
+	{
+		Matrix state = getState(in, out);
+		Matrix location; // get subarray of state (unimplemented)
+
+		move(out, location, target);
+
+		if ((target - location).magnitude() < minDistance)
+			close = true;
+	}
+	return true;
+}
 
