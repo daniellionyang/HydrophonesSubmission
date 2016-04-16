80e69abcf700d0100d9fb20a03736cc9ebebe216
Luke Shimanuki <luke.shimanuki@gmail.com>
2016-04-15 17:49:44 -0700

    connection functions can save state between iterations

--- src/interface/functions.cpp
+++ src/interface/functions.cpp
@@ -12,240 +12,260 @@
 
 bool camera_f(FILE* in, FILE* out, Data* data)
 {
-	fprintf(out, "i\n"); // request image
-	auto img = imageRead(in); // read image
+	bool quit;
+	while (!quit)
+	{
+		fprintf(out, "i\n"); // request image
+		auto img = imageRead(in); // read image
 
-	// store image
-	data->lock();
-		data->imageFront = std::move(img);
-		data->imageFrontID++;
-	data->unlock();
+		// store image
+		data->lock();
+			data->imageFront = std::move(img);
+			data->imageFrontID++;
+		data->unlock();
 
-	std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
 
 	return true;
 }
 
 bool camera_d(FILE* in, FILE* out, Data* data)
 {
-	fprintf(out, "i\n"); // request image
-	auto img = imageRead(in); // read image
+	bool quit;
+	while (!quit)
+	{
+		fprintf(out, "i\n"); // request image
+		auto img = imageRead(in); // read image
 
-	// store image
-	data->lock();
-		data->imageDown = std::move(img);
-		data->imageDownID++;
-	data->unlock();
+		// store image
+		data->lock();
+			data->imageDown = std::move(img);
+			data->imageDownID++;
+		data->unlock();
 
-	std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
 
 	return true;
 }
 
 bool mission(FILE* in, FILE* out, Data* data)
 {
-	char m;
-	fscanf(in, " %c", &m);
-	switch (m)
+	bool quit;
+	while (!quit)
 	{
-		case 'q': // query
+		char m;
+		fscanf(in, " %c", &m);
+		switch (m)
 		{
-			char q;
-			fscanf(in, " %c", &q);
-			switch (q)
+			case 'q': // query
 			{
-				case 's': // state
+				char q;
+				fscanf(in, " %c", &q);
+				switch (q)
 				{
-					data->lock();
-						auto state = data->state;
-					data->unlock();
-
-					state.write(out);
-					break;
-				}
-				case 'i': // image
-				{
-					char i;
-					fscanf(in, " %c", &i);
-					switch (i)
+					case 's': // state
 					{
-						case 'f': // front
-						{
-							data->lock();
-								auto img = data->imageFront;
-							data->unlock();
+						data->lock();
+							auto state = data->state;
+						data->unlock();
 
-							imageWrite(out, img);
-							break;
-						}
-						case 'd': // down
-						{
-							data->lock();
-								auto img = data->imageDown;
-							data->unlock();
-
-							imageWrite(out, img);
-							break;
-						}
+						state.write(out);
+						break;
 					}
-					break;
-				}
-				case 'm': // modeling
-				{
-					char m;
-					fscanf(in, " %c", &m);
-					switch (m)
+					case 'i': // image
 					{
-						case 'm': // mode
+						char i;
+						fscanf(in, " %c", &i);
+						switch (i)
 						{
-							data->lock();
-								auto model = data->model;
-							data->unlock();
+							case 'f': // front
+							{
+								data->lock();
+									auto img = data->imageFront;
+								data->unlock();
 
-							model.write(out);
-							break;
+								imageWrite(out, img);
+								break;
+							}
+							case 'd': // down
+							{
+								data->lock();
+									auto img = data->imageDown;
+								data->unlock();
+
+								imageWrite(out, img);
+								break;
+							}
 						}
-						case 's': // system
-						{
-							System().write(out); // currently unsupported
-							break;
-						}
-						case 'c': // certainty
-						{
-							fprintf(out, "0\n"); // currently unsupported
-							break;
-						}
+						break;
 					}
-					break;
+					case 'm': // modeling
+					{
+						char m;
+						fscanf(in, " %c", &m);
+						switch (m)
+						{
+							case 'm': // mode
+							{
+								data->lock();
+									auto model = data->model;
+								data->unlock();
+
+								model.write(out);
+								break;
+							}
+							case 's': // system
+							{
+								System().write(out); // currently unsupported
+								break;
+							}
+							case 'c': // certainty
+							{
+								fprintf(out, "0\n"); // currently unsupported
+								break;
+							}
+						}
+						break;
+					}
 				}
+				break;
 			}
-			break;
-		}
-		case 'c': // command
-		{
-			char c;
-			fscanf(in, " %c", &c);
-			switch(c)
+			case 'c': // command
 			{
-				case 's': // state
+				char c;
+				fscanf(in, " %c", &c);
+				switch(c)
 				{
-					auto state = State(in);
-					data->lock();
-						data->desiredState = std::move(state);
-						data->setState = true;
-					data->unlock();
-					break;
-				}
-				case 'd': // dropper
-					data->lock();
-						data->drop = true;
-					data->unlock();
-					break;
-				case 'g': // grabber
-				{
-					char g;
-					fscanf(in, " %c", &g);
-					switch (g)
+					case 's': // state
 					{
-						case 'g': // grab
-							data->lock();
-								data->grab = true;
-							data->unlock();
-							break;
-						case 'r': // release
-							data->lock();
-								data->release = true;
-							data->unlock();
-							break;
+						auto state = State(in);
+						data->lock();
+							data->desiredState = std::move(state);
+							data->setState = true;
+						data->unlock();
+						break;
 					}
-					break;
+					case 'd': // dropper
+						data->lock();
+							data->drop = true;
+						data->unlock();
+						break;
+					case 'g': // grabber
+					{
+						char g;
+						fscanf(in, " %c", &g);
+						switch (g)
+						{
+							case 'g': // grab
+								data->lock();
+									data->grab = true;
+								data->unlock();
+								break;
+							case 'r': // release
+								data->lock();
+									data->release = true;
+								data->unlock();
+								break;
+						}
+						break;
+					}
+					case 't': // torpedo
+					{
+						char t;
+						fscanf(in, " %c", &t);
+						data->lock();
+							data->shoot = t;
+						data->unlock();
+						break;
+					}
 				}
-				case 't': // torpedo
-				{
-					char t;
-					fscanf(in, " %c", &t);
-					data->lock();
-						data->shoot = t;
-					data->unlock();
-					break;
-				}
+				break;
 			}
-			break;
 		}
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
-
-	std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
 	return true;
 }
 
 bool modeling(FILE* in, FILE* out, Data* data)
 {
-	fprintf(out, "m\n"); // request model
-	auto model = Matrix(in); // read model
-
-	std::queue<System> evidence;
-	// store model and get new evidences
-	data->lock();
-		data->model = std::move(model);
-		data->modelID++;
-		std::swap(evidence, data->evidence);
-	data->unlock();
-
-	// send new evidence
-	while (!evidence.empty())
+	bool quit;
+	while (!quit)
 	{
-		fprintf(out, "e\n");
-		evidence.front().write(out);
-		evidence.pop();
-	}
+		fprintf(out, "m\n"); // request model
+		auto model = Matrix(in); // read model
 
-	std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		std::queue<System> evidence;
+		// store model and get new evidences
+		data->lock();
+			data->model = std::move(model);
+			data->modelID++;
+			std::swap(evidence, data->evidence);
+		data->unlock();
+
+		// send new evidence
+		while (!evidence.empty())
+		{
+			fprintf(out, "e\n");
+			evidence.front().write(out);
+			evidence.pop();
+		}
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
 
 	return true;
 }
 
 bool control(FILE* in, FILE* out, Data* data)
 {
-	fprintf(out, "g\n"); // request state
-	auto state = State(in); // read state
-
-	State desiredState;
-	bool setState, drop, grab, release;
-	char shoot;
-	// store state and get new commands
-	data->lock();
-		data->state = std::move(state);
-		data->stateID++;
-
-		std::swap(desiredState, data->desiredState);
-		setState = data->setState;
-		data->setState = false;
-		shoot = data->shoot;
-		data->shoot = false;
-		drop = data->drop;
-		data->drop = false;
-		grab = data->grab;
-		data->grab = false;
-		release = data->release;
-		data->release = false;
-	data->unlock();
-
-	// send commands to arduino
-	if (setState)
+	bool quit;
+	while (!quit)
 	{
-		fprintf(out, "s\n");
-		desiredState.write(out);
+		fprintf(out, "g\n"); // request state
+		auto state = State(in); // read state
+
+		State desiredState;
+		bool setState, drop, grab, release;
+		char shoot;
+		// store state and get new commands
+		data->lock();
+			data->state = std::move(state);
+			data->stateID++;
+
+			std::swap(desiredState, data->desiredState);
+			setState = data->setState;
+			data->setState = false;
+			shoot = data->shoot;
+			data->shoot = false;
+			drop = data->drop;
+			data->drop = false;
+			grab = data->grab;
+			data->grab = false;
+			release = data->release;
+			data->release = false;
+		data->unlock();
+
+		// send commands to arduino
+		if (setState)
+		{
+			fprintf(out, "s\n");
+			desiredState.write(out);
+		}
+
+		if (shoot == 'r') fprintf(out, "t r\n");
+		if (shoot == 'l') fprintf(out, "t l\n");
+
+		if (drop) fprintf(out, "d\n");
+		if (grab) fprintf(out, "g\n");
+		if (release) fprintf(out, "r\n");
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
-
-	if (shoot == 'r') fprintf(out, "t r\n");
-	if (shoot == 'l') fprintf(out, "t l\n");
-
-	if (drop) fprintf(out, "d\n");
-	if (grab) fprintf(out, "g\n");
-	if (release) fprintf(out, "r\n");
-
-	std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
 	return true;
 }
