22b697246c4f0782cc7a1abb86b0a0148320e8ec
Luke Shimanuki <luke@luke.shimanuki@gmail.com>
2016-04-17 14:30:32 -0700

    add conversion from matrix to model

--- src/mission/action.cpp
+++ src/mission/action.cpp
@@ -2,6 +2,46 @@
 #include "mission/command.hpp"
 #include "mission/query.hpp"
 
+struct Model
+{
+	Model(Matrix vec)
+	{
+		rBuoy[0] = vec.get(0);
+		rBuoy[1] = vec.get(1);
+		rBuoy[2] = vec.get(2);
+		gBuoy[0] = vec.get(3);
+		gBuoy[1] = vec.get(4);
+		gBuoy[2] = vec.get(5);
+		yBuoy[0] = vec.get(6);
+		yBuoy[1] = vec.get(7);
+		yBuoy[2] = vec.get(8);
+
+		pvc[0] = vec.get(9);
+		pvc[1] = vec.get(10);
+		pvc[2] = vec.get(11);
+
+		board[0] = vec.get(12);
+		board[1] = vec.get(13);
+		board[2] = vec.get(14);
+
+		coveredBin[0] = vec.get(15);
+		coveredBin[1] = vec.get(16);
+		openBin[0] = vec.get(17);
+		openBin[1] = vec.get(18);
+
+		pinger[0] = vec.get(19);
+		pinger[1] = vec.get(20);
+	}
+
+	std::array<float, 3>
+		rBuoy, gBuoy, yBuoy,
+		pvc, // maneuvering
+		board; // torpedoes
+	std::array<float, 2>
+		coveredBin, openBin,
+		pinger;
+};
+
 Wait::Wait(long long time) :
 	wtime(time)
 {
