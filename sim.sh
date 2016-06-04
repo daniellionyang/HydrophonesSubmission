84f57402ba7e033a237d92ef36a79448281b5111
Luke Shimanuki <luke@luke.shimanuki@gmail.com>
2016-06-04 14:21:23 -0700

    interface connects to fake camera

--- sim.sh
+++ sim.sh
@@ -3,8 +3,11 @@
 trap "exit" INT TERM
 trap "echo caught exit signal; kill 0" EXIT
 
-bin/image_show < pipe/render_f &
-bin/image_show < pipe/render_d &
+tee pipe/image_f < pipe/render_f | bin/image_show &
+tee pipe/image_d < pipe/render_d | bin/image_show &
+
+bin/camera_pipe pipe/image_f < pipe/camera_f_in > pipe/camera_f_out &
+bin/camera_pipe pipe/image_d < pipe/camera_d_in > pipe/camera_d_out &
 
 bin/sim_state < pipe/control_in > pipe/control_out
 
--- src/interface/config.cpp
+++ src/interface/config.cpp
@@ -4,10 +4,13 @@ std::vector<Connection> defaultConnections()
 {
 	return
 	{
-		// function  input                output
+		// function  input                output              args
 		{  control,  "pipe/control_out",  "pipe/control_in"  },
 		{  mission,  "pipe/mission_out",  "pipe/mission_in"  },
 		{  modeling, "pipe/modeling_out", "pipe/modeling_in" },
+
+		{  camera,   "pipe/camera_f_out", "pipe/camera_f_in", I_FRONT },
+		{  camera,   "pipe/camera_d_out", "pipe/camera_d_in", I_DOWN  },
 	};
 }
 
