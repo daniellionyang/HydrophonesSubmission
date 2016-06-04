58afc9652a840697d16373fc1448fed3541c87a1
Evan McNeil <evanmcneil1999@gmail.com>
2016-06-04 15:36:59 -0700

    change int to size_t

--- include/interface/functions.hpp
+++ include/interface/functions.hpp
@@ -9,7 +9,7 @@ bool hydrophones(Data*, const std::string, const std::string);
 
 bool vision(Data*, const std::string, const std::string, size_t, size_t);
 
-bool camera(Data*, const std::string, const std::string, int);
+bool camera(Data*, const std::string, const std::string, size_t);
 
 bool mission(Data*, const std::string, const std::string);
 bool modeling(Data*, const std::string, const std::string);
