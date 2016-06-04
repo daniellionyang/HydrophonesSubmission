3a81170d0b30655821cbf13f297300f79fb3e16e
Luke Shimanuki <luke@luke.shimanuki@gmail.com>
2016-06-04 12:52:38 -0700

    switch from piping to io redirection

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 trap "exit" INT TERM
 trap "echo caught exit signal; kill 0" EXIT
 
-cat pipe/mission_in  | bin/mission  | cat > pipe/mission_out  &
-cat pipe/modeling_in | bin/modeling | cat > pipe/modeling_out &
+bin/mission  < pipe/mission_in  > pipe/mission_out  &
+bin/modeling < pipe/modeling_in > pipe/modeling_out &
 
 bin/interface
 
--- sim.sh
+++ sim.sh
@@ -3,8 +3,8 @@
 trap "exit" INT TERM
 trap "echo caught exit signal; kill 0" EXIT
 
-cat pipe/render_f | bin/image_show &
-cat pipe/render_d | bin/image_show &
+bin/image_show < pipe/render_f &
+bin/image_show < pipe/render_d &
 
-cat pipe/control_in | bin/sim_state | cat > pipe/control_out
+bin/sim_state < pipe/control_in > pipe/control_out
 
