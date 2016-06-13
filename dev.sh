e156c9357a37756ecc16d2f5c3e22ecd00deeef8
Marlin <hello@avbotz.com>
2016-06-13 13:55:26 -0700

    control_in and control_out logs are timestamped

--- dev.sh
+++ dev.sh
@@ -3,12 +3,14 @@
 trap "exit" INT TERM
 trap "echo caught exit signal; kill 0" EXIT
 
+F='%Y-%m-%d_%H-%M-%S'
+
 stty -F /dev/ttyUSB0 raw
 stty -F /dev/ttyUSB0 speed 9600
 stty -F /dev/ttyUSB0 -echo -echoe -echok
 
-cat /dev/ttyUSB0    | tee pipe/control_out >> logs/control_out &
-cat pipe/control_in | tee /dev/ttyUSB0     >> logs/control_in  &
+cat /dev/ttyUSB0    | tee pipe/control_out | ts ${F} >> logs/control_out &
+cat pipe/control_in | tee /dev/ttyUSB0     | ts ${F} >> logs/control_in  &
 
 bin/camera i 0 1 < pipe/camera_f_in | tee pipe/camera_f_out | bin/image_log logs/ _f &
 
