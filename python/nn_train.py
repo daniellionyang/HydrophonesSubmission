6f2b5917c4b14c612368f5b4cb84aa57d5eeaca5
Luke Shimanuki <luke.shimanuki@gmail.com>
2016-07-16 13:54:10 -0700

    add python neural network tools

---
+++ python/nn_train.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+
+import numpy as np
+
+from lib.neural_network import NeuralNetwork
+
+filename = sys.argv[1]
+
+numInputs = 3
+numFeatures = 4
+numOutputs = 1
+
+inputs = np.array([
+], dtype=np.float32) / 256
+targets = np.array([[1]]*0 + [[0]]*0, dtype=np.float32)
+
+nn = NeuralNetwork(numInputs, numFeatures, numOutputs)
+
+nn.load('/ram/weights.npz')
+
+while True:
+	nn.train(inputs, targets)
+
+	if i % 256 == 0:
+		print("{} {}".format(int(i/256), nn.evaluate(inputs, targets)))
+	if i % (256**2) == 0:
+		nn.save(filename)
+		print("{}".format(nn.session().run(nn.weights())))
+
