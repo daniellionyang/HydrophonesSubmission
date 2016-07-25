c6237c13718b20621cb954a06d04025ccf5d74d4
Evan McNeil <evanmcneil1999@gmail.com>
2016-07-25 13:10:11 -0700

    added real-time image displaying

--- dev.sh
+++ dev.sh
@@ -24,7 +24,12 @@ stty -F ${FPGA} -echo -echoe -echok
 
 cat ${FPGA} | tee pipe/hydrophones_in >> logs/hydrophones_in &
 
-bin/camera i 0 1 < pipe/camera_f_in | tee pipe/camera_f_out | bin/image_log logs/ _f &
-bin/camera i 1 0 < pipe/camera_d_in | tee pipe/camera_d_out | bin/image_log logs/ _d &
+DISPLAY=localhost:10.0
+
+bin/camera i ${FRONT} 1 < pipe/camera_f_in | tee pipe/camera_f_out | bin/image_log logs/ _f &
+bin/camera i ${DOWN}  0 < pipe/camera_d_in | tee pipe/camera_d_out | bin/image_log logs/ _d &
+
+#bin/camera i ${FRONT} 1 < pipe/camera_f_in | tee pipe/camera_f_out > >(bin/image_show 1) | bin/image_log logs/ _f &
+#bin/camera i ${DOWN}  0 < pipe/camera_d_in | tee pipe/camera_d_out > >(bin/image_show 1) | bin/image_log logs/ _d &
 
 cat
